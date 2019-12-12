7e51655ca7a0c0b4bd5f7c54c4289221e62d0a94
marinacarradore@gmail.com <marinacarradore@gmail.com>
2019-12-12 16:19:59 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursomc/services/CategoriaService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import com.cursomc.domain.Categoria;
 import com.cursomc.dto.CategoriaDTO;
@@ -44,6 +46,12 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage);
+		return repo.findAll(pageRequest);
+		
 	}
 
 }
